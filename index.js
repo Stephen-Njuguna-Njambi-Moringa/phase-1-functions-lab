37413b3f446fdb7bfe288295c7394786cd75936b
Stephen Njuguna Njambi <stepehen.jambi@student.moringaschool.com>
2022-08-20 16:43:19 +0300

    lab passed

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+
+//returns the number of blocks given a value
+function distanceFromHqInBlocks (blockValue) {
+  if (blockValue<42){
+    return(42-blockValue);
+  }
+  else {
+  return(blockValue-42);
+}
+}
+
+
+function distanceFromHqInFeet (blockValue) {
+  return distanceFromHqInBlocks(blockValue)*264;
+}
+
+function distanceTravelledInFeet(a,b){
+  if (b>a){
+    return((b-a)*264);
+  }
+  else
+    return((a-b)*264);
+}
+
+
+function calculatesFarePrice(start,destination){
+  let distance = distanceTravelledInFeet(start,destination);
+  if (distance<= 400){
+    return 0;
+  }
+  if (distance>=400 && distance<=2000){
+    return (distance-400)* 0.02;
+  }
+  else if(distance>=2000&& distance<=2500){
+    return 25;
+  }
+  else if (distance > 2500){
+    return "cannot travel that far";
+  }
+}
+
+
